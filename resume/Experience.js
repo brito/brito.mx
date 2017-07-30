514c2118a63270c5003d3de63d46b1ef48701aba
brito <brito@1618033.local>
2017-07-30 15:17:16 -0700

    Experience parser WIP

--- resume/Experience.js
+++ resume/Experience.js
@@ -1,23 +1,38 @@
 Experience = 
 	([text]) => text
-		.trim().split(/\n{2,}/)
+		.trim()
+		.split(/\n{2,}/)
 		// each
 		.map(role => role
 			// trim
-			.split(/\n/).map(line => line.trim()).join('\n')
+			.split(/\n/)
+			.map(line => line.trim())
+			.join('\n')
 			// title
-			.replace(/^.+ \| /, m => console.warn(m)||'')
-			// locations
-			.replace(/.+\n/, m => console.warn(m)||'')
+			.replace(/^(.+)(?= \| )/, 
+				(text,title) => console.group(`<section> ${title}`)||'')
+			// location
+			.replace(/ \| (.+)/, 
+				(text,url) => console.log(url.link(url))||'')
 			// dates
-			.replace(/^\d.+\n/u, m => console.warn(m)||'')
+			.replace(/\n\s*([-\d]+)[ \D]*([-\d]+)/u, 
+				(text,from,to) => console.warn(new Date(from),new Date(to))||'')
 			// highlights
-			.replace(/•.+\n/g, m => console.warn(m)||'')
+			.replace(/\n\s*•.+/g, 
+				m => console.warn(m)||'')
 			// skills
-			.replace(/\[\s([^\]]+)\s\]/, skills)
-	)
-
-
-function skills(all, list){
-	return console.warn(list.split` `)
-}
+			.replace(/\[\s([^\]]+)\s\]/, 
+				(text,list) => list
+					.split`, `
+					//
+					.reduce((accumulator, label, i, {length}) => 
+						accumulator.concat([[ label, (length-i)/length ]])
+					, [])
+					//
+					// .forEach(([label, weight]) => {
+					// 	console.error(label, weight)
+					// })
+			)
+			.replace(/$/, ()=>console.log('</section>')||console.groupEnd()||'')
+		)
+		.reduce((all, one)=> console.log(one) || all.concat(one) , []);
