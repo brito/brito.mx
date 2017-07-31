6e8f04864e4fb98c75ac74262089239dc60e6add
brito <brito@1618033.local>
2017-07-30 19:27:03 -0700

    Parsing WIP

--- resume/Experience.js
+++ resume/Experience.js
@@ -10,16 +10,19 @@ Experience =
 			.join('\n')
 			// title
 			.replace(/^(.+)(?= \| )/, 
-				(text,title) => console.group(`<section> ${title}`)||'')
+				(text,title) => console.group(title)
+					|| `\n<article title="${title}">` )
 			// location
 			.replace(/ \| (.+)/, 
-				(text,url) => console.log(url.link(url))||'')
-			// dates
-			.replace(/\n\s*([-\d]+)[ \D]*([-\d]+)/u, 
-				(text,from,to) => console.warn(new Date(from),new Date(to))||'')
+				(text,url) => console.log(url) 
+					|| `\n${url.link(url)}` )
+			// duration
+			.replace(/\n\s*([-\d]+) \D ([-\d]+)/u, 
+				(text,from,to) => console.log(`${from} to ${to}`) 
+					|| `\n<time datetime=${(new Date(to) - new Date(from)) / (1e3*60*60*24*365/2080)}h>${from} to ${to}</time>`)
 			// highlights
-			.replace(/\n\s*•.+/g, 
-				m => console.warn(m)||'')
+			.replace(/\n\s*•(.+)/g, 
+				(text, highlight) => console.warn(highlight) || `\n<li> ${highlight}`)
 			// skills
 			.replace(/\[\s([^\]]+)\s\]/, 
 				(text,list) => list
@@ -27,12 +30,9 @@ Experience =
 					//
 					.reduce((accumulator, label, i, {length}) => 
 						accumulator.concat([[ label, (length-i)/length ]])
-					, [])
-					//
-					// .forEach(([label, weight]) => {
-					// 	console.error(label, weight)
-					// })
-			)
-			.replace(/$/, ()=>console.log('</section>')||console.groupEnd()||'')
+					, []))
+			// end
+			.replace(/$/, ()=>console.groupEnd()||'\n</article>')
 		)
+		// end
 		.reduce((all, one)=> console.log(one) || all.concat(one) , []);
