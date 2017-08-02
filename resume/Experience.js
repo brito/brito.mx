badf93ff51fba3b87d20933345945acbdec858ad
brito <brito@1618033.local>
2017-08-01 17:57:53 -0700

    Preparing to consume (expression, transformation) tuples per line parsed

--- resume/Experience.js
+++ resume/Experience.js
@@ -1,42 +1,47 @@
 
-Experience = 
+Experience =
 	([text]) => text
-		.trim()
 		.split(/\n{2,}/)
 		// each
 		.map(role => role
-			// trim
+			// trim whole
+			.trim()
+			// ...and each line
 			.split(/\n/)
-			.map(line => line.trim())
+			.map(line => line
+				.trim()
+				// title
+				.replace(/^(.+)(?= \| )/, 
+					(text,title) => console.group(title)
+						|| `\n<article title="${title}">` )
+				// location
+				.replace(/ \| (.+)/, 
+					(text,url) => console.log(url) 
+						|| `\n${url.link(url)}` )
+
+				// duration
+				.replace(/([-\d]+) \D ([-\d]+)/u, 
+					(text,from,to) => console.log(`${from} to ${to}`) 
+						|| `<time datetime=${(new Date(to) - new Date(from)) / (1e3*60*60*24*365/2080)}h>${from} to ${to}</time>`)
+				// items
+				.replace(/•(.+)/g, 
+					(text, item) => console.warn(item) || `<li> ${item}`)
+				// tags
+				.replace(/\[\s([^\]]+)\s\]/, 
+					(text,list) => list
+						.split`, `
+						// assign decreasing relative values
+						.reduce((accumulator, label, i, {length}) => 
+							accumulator.concat([[ label, (length-i)/length ]])
+						, [])
+						// map output
+						.map(([label, weight]) => console.log(label, weight) 
+							|| `<label data=${ weight.toFixed(2) }> ${label} </label>`))
+			)
 			.join('\n')
-			// title
-			.replace(/^(.+)(?= \| )/, 
-				(text,title) => console.group(title)
-					|| `\n<article title="${title}">` )
-			// location
-			.replace(/ \| (.+)/, 
-				(text,url) => console.log(url) 
-					|| `\n${url.link(url)}` )
-			// duration
-			.replace(/\n\s*([-\d]+) \D ([-\d]+)/u, 
-				(text,from,to) => console.log(`${from} to ${to}`) 
-					|| `\n<time datetime=${(new Date(to) - new Date(from)) / (1e3*60*60*24*365/2080)}h>${from} to ${to}</time>`)
-			// items
-			.replace(/\n\s*•(.+)/g, 
-				(text, item) => console.warn(item) || `\n<li> ${item}`)
-			// tags
-			.replace(/\[\s([^\]]+)\s\]/, 
-				(text,list) => list
-					.split`, `
-					// assign decreasing relative values
-					.reduce((accumulator, label, i, {length}) => 
-						accumulator.concat([[ label, (length-i)/length ]])
-					, [])
-					// map output
-					.map(([label, weight]) => console.log(label, weight) 
-						|| `<label data=${weight.toFixed(2)}> ${label} </label>`))
 			// end
 			.replace(/$/, ()=>console.groupEnd()||'\n</article>')
 		)
 		// join
-		.reduce((all, one)=> all.concat(one) , []);
+		.reduce((all, one)=> all.concat(one), [])
+		.forEach(item => console.log(item));
