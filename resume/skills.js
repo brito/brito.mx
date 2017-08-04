b21fcc85ed4b23bc6a58902878dae991589d07fc
brito <brito@1618033.local>
2017-08-03 18:42:06 -0700

    Content fit adjust + wording

--- resume/skills.js
+++ resume/skills.js
@@ -7,32 +7,32 @@ let accumulator = {},
             at: 'Appian',
             from: '2002-12',
             to: '2007-03',
-            skills: 'JS/ES6 jQuery UX CSS3 HTML5 Recruiting Regex REST/JSON Git/SVN/Hg/P4 Java/J2EE Portal BPM XML a11y/WCAG2 i18n/l16n'
+            skills: 'JS/ES6 jQuery UX CSS3 HTML5 Recruiting Regex REST/JSON Git/SVN Java/J2EE Portal BPM XML a11y/WCAG2 i18n/l16n'
         },{
             at: 'Google',
             from: '2007-03',
             to: '2009-09',
-            skills: 'CSS3 HTML5 JS/ES6 UX bash C++ Git/SVN/Hg/P4 UX latency Python Recruiting'
+            skills: 'CSS3 HTML5 JS/ES6 UX bash C++ P4 UX latency Python Recruiting'
         },{
             at: 'Gravity',
             from: '2009-11',
             to: '2011-02',
-            skills: 'JS/ES6 jQuery CSS3 HTML5 PHP UX Git/SVN/Hg/P4 dataviz REST/JSON'
+            skills: 'JS/ES6 jQuery CSS3 HTML5 PHP UX Git/SVN dataviz REST/JSON'
         },{
             at: 'Small Demons',
             from: '2011-10',
             to: '2012-11',
-            skills: 'jQuery CSS3 HTML5 UX JS/ES6 Git/SVN/Hg/P4 Python Typography'
+            skills: 'jQuery CSS3 HTML5 UX JS/ES6 Hg Python Typography'
         },{
             at: 'Intertrust',
             from: '2012-12',
             to: '2013-04',
-            skills: 'JS/ES6 CSS3 HTML5 UX Git/SVN/Hg/P4 BackboneJS'
+            skills: 'JS/ES6 CSS3 HTML5 UX Git/SVN BackboneJS'
         },{
             at: 'Oracle',
             from: '2013-04',
             to: '2017-07',
-            skills: 'UX Sales MADP/MBaaS Typography HTML5 CSS3 JS/ES6 Java/J2EE Git/SVN/Hg/P4 SVG bash FCPX Portal dataviz a11y/WCAG2 Cordova Nodejs SaaS/PaaS BPM'
+            skills: 'UX Sales MADP/MBaaS Typography HTML5 CSS3 JS/ES6 Java/J2EE Git/SVN SVG bash Nodejs FCPX Portal dataviz a11y/WCAG2 Cordova SaaS/PaaS BPM'
         }
     ]
     //
@@ -67,7 +67,7 @@ Object.entries(accumulator)
             data = document.createElement`data`;
 
         label.innerHTML = ` ${skill[0]}`;
-        data.innerHTML = `${Array(Math.ceil(weight)).join('&#9632;')} ${weight > 5 ? Math.floor(weight) : ''}`;
+        data.innerHTML = `${Array(Math.ceil(weight)).join('|')} ${weight > 5 ? Math.floor(weight) : ''}`;
         data.style.cssText = `color:hsl(${color},62%,50%); white-space:nowrap; margin-left:1ex`;
         label.appendChild(data);
 
