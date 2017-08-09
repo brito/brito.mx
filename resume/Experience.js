b176501b752f5e5984442b82feec5215d60fb777
brito <brito@1618033.local>
2017-08-09 16:08:05 -0700

    This is the basic algorithm, now to clean it up with more abstractions

--- resume/Experience.js
+++ resume/Experience.js
@@ -2,73 +2,67 @@
 Experience =
 	([text]) => text
 		.split(/\n{2,}/)
-		.reduce((list, text) => {
-		    // transform text into item of list
-			let item = {},
-			    // text is matched and substituted
-				substitutions = 
-				[
-				    // location / title
-				    [/(.+) \/ (.+?)\n/,
-				        (text, href, title)=>({
-				            'article[title]': title,
-				            'a[href]': href
-				        })],
 
-				    // duration
-			        [/([-\d]+) \D ([-\d]+)\n/u, 
-			            (text, from, to) => ({ 'time[datetime]': `${from} — ${to}` })],
+		.map(article => {
+		    let el = document.createElement`article`;
+		    [   // location / title		      
+                [/(.+) \/ (.+?)\n/,
+                    (text, href, title)=>{
+                        el.title = title;
+                        el.innerHTML = href.link(`https://${href}`);
+                    }],
 
-			        // remarks
-			        [/•\s+(.+)\n/g, 
-			            (text, li) => ({ 'li': li })],
+                // duration
+                [/([-\d]+) \D ([-\d]+)\n/u, 
+                    (text, from, to) => {
+                    	let time = document.createElement`time`,
+                    		duration  = new Date(to) - new Date(from);
+                    	//w3c.github.io/html/infrastructure.html#duration-time-component-scale
+                    	const W = 6048e5;
+                        time.datetime = `${duration * W} W`;
+                        time.innerText = `${from} — ${to}`;
+                        el.appendChild(time);
+                    }],
 
-			        // skills
-			        [/\[\s([^\]]+)\s\]/, 
-			            (text, label) => ({ 'label[data]': label })]
-			    ]
+                // remarks
+                [/•\s+(.+)\n/g, 
+                    (text, remark) => {
+                    	let ul = el.querySelector`ul` || document.createElement`ul`,
+                    		li = document.createElement`li`;
+                    	li.innerText = remark;
+                    	ul.appendChild(li);
+                    	el.appendChild(ul);
+                    }],
 
-			    // consume text substitutions
-			    .reduce((text, [re, fn]) => 
-			         text.replace(re, (...values) => {
-			    		let result = fn(...values);
-			    		Object
-			    		   .entries(result)
-			    		   .forEach(([selector, value]) => {
-                                   item[selector] = item[selector] ?
-                                    [].concat(item[selector], value)
-                                    : value;
-			    		   });
-			    		return result;
-			    	})
-			    , text);
+                // skills
+                [/\[\s([^\]]+)\s\]/, 
+                    (text, skills) => { 
+                    	skills
+                    		.split`, `
+                    		// assign decreasing relative values
+                    		.reduce((accumulator, label, i, {length}) =>
+                    			accumulator.concat([[label, (length-i)/length ]])
+                    		, [])
+                    		// map output
+                    		.map(([label, weight]) => {
+                    			let data = document.createElement`data`;
+								data.setAttribute('data', weight);
+								data.innerText = label;
+								el.appendChild(data);
+                    		});
+                    }]
+            ]
+            // transform succinctly
+            .reduce((text, [re,fn]) => {
+                return text.replace(re, fn);
+            }, article);
+		    return el;
+		})
 
-			return list.concat(item);
-		}
-		,[])
-		//
-		.map(item => {
-		    let root;
-		    Object
-		      .entries(item)
-		      .forEach(([selector, value], i) => {
-		          const [, name, attr] = /^(\w+)\[?([^\]]*)\]?/.exec(selector);
-		          let el = document.createElement(name);
-		          if (attr)
-		              el.setAttribute(attr, value);
-		          else if (Array.isArray(value))
-		              ;
-		          else
-		              el.innerHTML = value;
-
-		          if (!i)
-		              root = el;
-		          else
-		              root.appendChild(el);
-		      });
-		    console.info(item)
-		    return root;
-		});
+		.reduce((section, article) => {
+		    section.appendChild(article);
+		    return section;
+		}, document.createElement`section`);
 
 /** /
 Experience =
