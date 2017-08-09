feaccff1d4f1de4dc72d0393a5607c30c5540842
brito <brito@1618033.local>
2017-08-09 16:32:13 -0700

    Cleanup, replace text

--- resume/Experience.js
+++ resume/Experience.js
@@ -1,4 +1,4 @@
-/**/// WIP intended to replace skills.js when finished
+/**/// WIP replacing skills.js
 Experience =
 	([text]) => text
 		.split(/\n{2,}/)
@@ -59,61 +59,15 @@ Experience =
 		    return el;
 		})
 
-		.reduce((section, article) => {
+		.reduce(([section], article) => {
 		    section.appendChild(article);
-		    return section;
-		}, document.createElement`section`);
+		    return [section];
+		}, [document.createElement`section`].map(el => {
+			el.className = 'Experience';
+			return el;
+		}))
 
-/** /
-Experience =
-	([text]) => text
-		.split(/\n{2,}/)
-		// each
-		.map(role => role
-			// trim whole
-			.trim()
-			// ...and each line
-			.split(/\n/)
-			.map(line => line
-				.trim()
-				// title
-				.replace(/^(.+)(?= \| )/, 
-					(text,title) => console.group(title)
-						|| `\n<article title="${title}">` )
-				// location
-				.replace(/ \| (.+)/, 
-					(text,url) => console.log(url) 
-						|| `\n${url.link(url)}` )
-				// duration
-				.replace(/([-\d]+) \D ([-\d]+)/u, 
-					(text,from,to) => console.log(`${from} to ${to}`) 
-						|| `<time datetime=${(new Date(to) - new Date(from)) / (1e3*60*60*24*365/2080)}h>${from} to ${to}</time>`)
-				// items
-				.replace(/•(.+)/g, 
-					(text, item) => console.warn(item) || `<li> ${item}`)
-				// tags
-				.replace(/\[\s([^\]]+)\s\]/, 
-					(text,list) => list
-						.split`, `
-						// assign decreasing relative values
-						.reduce((accumulator, label, i, {length}) => 
-							accumulator.concat([[ label, (length-i)/length ]])
-						, [])
-						// map output
-						.map(([label, weight]) => console.log(label, weight) 
-							|| `<label data=${ weight.toFixed(2) }> ${label} </label>`))
-			)
-			.join('\n')
-			// end
-			.replace(/$/, ()=>console.groupEnd()||'\n</article>')
-		)
-		// join
-		.reduce((all, one)=> all.concat(one), [])
-		.join('\n').
-		replace(/^[\w\W]+$/, m => {
-			let section = document.createElement`section`;
-				section.className = 'Experience';
-			section.innerHTML = m; 
-			document.body.replaceChild(section, document.querySelector`script:not([src])`)
+		.forEach(section => {
+			let script = document.querySelector`script:not([src])`;
+			script.parentNode.replaceChild(section, script);
 		});
-/**/
